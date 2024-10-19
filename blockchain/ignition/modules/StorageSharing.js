da3e1993e916bbf1917c1badf8a906476e191c46
Łukasz Tshipenchko <dev@zxc.sx>
2024-10-19 16:39:57 +0500

    Fixed deploy bug

--- blockchain/ignition/modules/StorageSharing.js
+++ blockchain/ignition/modules/StorageSharing.js
@@ -2,18 +2,8 @@
 // Learn more about it at https://hardhat.org/ignition
 
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-const { ethers } = require("hardhat"); 
-
-const DEFAULT_MAX_STORAGE = 1000;
-const DEFAULT_SOCKER = "example.com:1234";
 
 module.exports = buildModule("StorageSharingModule", (m) => {
-  const max_storage = m.getParameter("maxStorage", DEFAULT_MAX_STORAGE);
-  const socket = m.getParameter("socket", DEFAULT_SOCKER);
-  const price = "0.01"; // hardcoded price
-
-  const priceInWei = ethers.parseEther(price);
-
   const StorageSharingContract = m.contract(
     "StorageSharing",
     [], 
@@ -21,9 +11,6 @@ module.exports = buildModule("StorageSharingModule", (m) => {
   );
 
   console.log(StorageSharingContract);
-  const publicServerTx = StorageSharingContract.publicServer(socket, max_storage, { value: priceInWei });
 
-  m.waitFor(publicServerTx);
-
-  return { StorageSharingContract, publicServerTx }; // include the contract and transaction information 
+  return { StorageSharingContract };
 });
